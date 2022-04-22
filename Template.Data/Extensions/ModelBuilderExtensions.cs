16f707e85fdccd1da58f4eaad8cf0e8812b41888
eduardolessasantos <eduardolessa2011@gmail.com>
2022-04-22 15:41:17 -0300

    Instalando e configurando o Entity Framework Core em um projeto .Net Core

---
+++ Template.Data/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template.Domain.Entities;
+
+namespace Template.Data.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static ModelBuilder SeedData(this ModelBuilder builder)
+        {
+            builder.Entity<User>()
+                .HasData(
+                new User { Id = Guid.Parse("ed391cc44a1448709a81812d22243dd1"), Name = "User Default", Email = "userdefault@template.com"}
+                ); 
+            
+            return builder;
+        }
+    }
+}
---
+++ Template.Data/Migrations/20220422183256_Insert Default User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Template.Data.Migrations
+{
+    public partial class InsertDefaultUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Name" },
+                values: new object[] { new Guid("ed391cc4-4a14-4870-9a81-812d22243dd1"), "userdefault@template.com", "User Default" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("ed391cc4-4a14-4870-9a81-812d22243dd1"));
+        }
+    }
+}
