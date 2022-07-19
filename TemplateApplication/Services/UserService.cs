536b79e43fe38fd7e8e020ea7985eccc0fa0a53b
eduardolessasantos <eduardolessa2011@gmail.com>
2022-07-19 16:17:47 -0300

    Criando API GetOne e Put

--- TemplateApplication/Services/UserService.cs
+++ TemplateApplication/Services/UserService.cs
@@ -27,26 +27,40 @@ namespace Template.Application.Services
             IEnumerable<User> _users = this.userRepository.GetAll();
 
             _userViewModels = mapper.Map<List<UserViewModel>>(_users);
-
-            //foreach (var item in _users )
-            //{
-            //    _userViewModels.Add(new UserViewModel { Id = item.Id, Email = item.Email, Name = item.Name });
-            //} 
             
             return _userViewModels;
         }
         public bool Post(UserViewModel userViewModel)
         {
-            //User _user = new User
-            //{
-            //    Id = Guid .NewGuid(),
-            //    Email = userViewModel.Email,
-            //    Name = userViewModel.Name
-            //};
 
             User _user = mapper.Map<User>(userViewModel);
 
             this.userRepository.Create(_user);
+
+            return true;
+        }
+
+        public UserViewModel GetById(string id)
+        {
+            if (!Guid.TryParse(id, out Guid userId))
+                throw new Exception("UserID is not valid");
+
+            User _user = this.userRepository.Find(x => x.Id == userId && !x.IsDeleted);
+            if (_user == null)
+                throw new Exception("User not found");
+
+            return mapper.Map<UserViewModel>(_user);
+        }
+
+        public bool Put(UserViewModel userViewModel)
+        {
+            User _user = this.userRepository.Find(x => x.Id == userViewModel.Id && !x.IsDeleted);
+            if (_user == null)
+                throw new Exception("User not found");
+
+            _user = mapper.Map<User>(userViewModel);
+
+            this.userRepository.Update(_user);
 
             return true;
         }
